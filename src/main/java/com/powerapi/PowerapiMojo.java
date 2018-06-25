cbe83106b8ba9bb45a1d16c4962c816d5a607609
Deblock Adrien <adrien.deblock@davidson.fr>
2018-06-25 11:49:54 +0200

    debug

--- src/main/java/com/powerapi/PowerapiMojo.java
+++ src/main/java/com/powerapi/PowerapiMojo.java
@@ -39,9 +39,11 @@ public class PowerapiMojo extends AbstractMojo {
     }
 
     public void executes() {
+        System.out.println("on execute..");
         String[] cmd = {"sh", "-c", "echo toto > untest.csv; (mvn test -DforkCount=0 | grep timestamp= | cut -d '-' -f 2 | tr -d ' ') > test1.csv & powerapi duration 30 modules procfs-cpu-simple monitor --frequency 50 --console --pids \\$! | grep muid) > data1.csv;"};
 
         try {
+            System.out.println("on execute..");
             Process p = Runtime.getRuntime().exec(cmd);
             getLog().info(readProc(p));
 
